dc2c8e8484cfaafb483503fb2224f57ac4c3572c
John Atkins <johnatkins1999@yahoo.com>
2017-07-06 12:33:36 -1000

    items added

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 /* jshint esversion:6*/
 const shoppingCart = document.getElementById('content');
 const title = document.getElementsByName('title');
+//console.log(title);console.log(title);
 const description = document.getElementsByName('description');
 let list = new ShoppingList();
 let renderOuput = list.render();
 shoppingCart.innerHTML = renderOuput;
 
 function add_to_shopping_list(){
-  var new_shopping_list_item = new ShoppingListItem(title.innerHTML, description.innerHTML);
-  console.log(title.innerHTML);
-  console.log(description.innerHTML);
+  var new_shopping_list_item = new ShoppingListItem(title[0].value, description[0].value);
   list.addItem(new_shopping_list_item);
   shoppingCart.innerHTML = list.render();
+  title[0].value = '';
+  description[0].value = '';
 }
 
 let button = document.getElementById('add_shopping_list_item_button');
